d73ab8ed0564df8fa16d1929ac2bffdedbb6b225
a121255 <appleybc@naver.com>
2020-06-18 11:48:46 +0900

    cinema vo

---
+++ src/main/java/com/tm/cgv/cinema/CinemaVO.java
@@ -0,0 +1,19 @@
+package com.tm.cgv.cinema;
+
+import lombok.Data;
+
+
+public class CinemaVO {
+
+	private int num;
+	
+	private String name;
+	private String local;
+	private String address;
+	private String tel;
+	private int totalTheater;
+	private int totalSeat;
+	private String trafficInfo;
+	private String parkingInfo;
+	
+}
