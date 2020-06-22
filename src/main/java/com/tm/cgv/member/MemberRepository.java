bb2bb4add04aa9f948c454a32ca0dc2e02e8a786
thdus3009 <thdus3009@gmail.com>
2020-06-22 10:13:32 +0900

    aa

--- src/main/java/com/tm/cgv/member/MemberRepository.java
+++ src/main/java/com/tm/cgv/member/MemberRepository.java
@@ -10,7 +10,8 @@ public interface MemberRepository {
 	
 	public int memberJoin(MemberVO memberVO) throws Exception;
 	
-	public MemberVO memberIdCheck(String id) throws Exception;
+	public MemberVO memberIdCheck(String uid) throws Exception;
+	public MemberVO memberEmailCheck(String email) throws Exception;
 	
 	public MemberVO memberSelect(MemberVO memberVO) throws Exception;
 	
